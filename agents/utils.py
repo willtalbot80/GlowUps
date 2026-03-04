e448976d8654958916741533b046a80dcbd6c493
willtalbot80 <willtalbot80@gmail.com>
2026-03-04 07:43:38 +0000

    Create utils.py with helper functions for agents

---
+++ agents/utils.py
@@ -0,0 +1,19 @@
+# utils.py
+
+"""
+This module contains helper functions and utilities for agents.
+"""
+
+
+def example_helper_function(param1, param2):
+    """
+    An example helper function that uses two parameters.
+    """
+    return param1 + param2
+
+
+def another_util_function(data):
+    """
+    An example utility function that processes some data.
+    """
+    return [item for item in data if item is not None]
